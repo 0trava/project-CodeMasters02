1180f917427405f72ca6703791dc15a53dde1d00
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-08-10 22:21:51 +0300

    added code

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
-
+import React, { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { SideBarPage } from 'pages/SideBarPage/SideBarPage';
+import css from './MainLayout.module.css';
+/*
+import
+{
+  Navigation,
+  Block, LogoBlock, LogoText, Text, PanelText,
+  PageBlock, PagePanel, PageSideBar, PageSideBarText,
+  Button, ButtonText,
+  FeedbackBlock, ButtonFeedbackText, AvatarUsers
+}
+from './MainLayout.styled';
+*/
 export const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <>
+      <header className={css.header}>
+        <aside className={css.navigation}>
+          <nav>
+            <SideBarPage></SideBarPage>
+          </nav>
+        </aside>
+      </header>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet/>
+      </Suspense>
+    </>
   )
 }
