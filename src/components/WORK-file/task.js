3508c2eda91b401c6e7f7fc0fd8880b04787b81f
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-08-10 21:26:14 +0300

    fix

---
+++ src/components/WORK-file/task.js
@@ -0,0 +1,14 @@
+[
+    {
+    name: "Kateryna Hilcher",
+    comment: "Really helps to plan the day and visualize tasks. Exactly what I was looking for. Huge thanks to the developers! "
+    },
+    {
+    name: "Mykola Kapytaniuk",
+    comment: "Very cool app, everything is clear and functional. A great program for time chronology."
+    },
+    {
+    name: "Ihor Dronenko",
+    comment: "Really helps to increase productivity. The application has a clear interface and useful features. I like it at the initial stage. "
+    },
+      ]
