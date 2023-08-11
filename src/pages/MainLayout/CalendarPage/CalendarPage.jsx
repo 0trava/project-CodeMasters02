1626272235251d51557d71ce93cf6a6b21ec4f1f
Demag0g1 <konievanatol@gmail.com>
2023-08-12 02:40:06 +0300

    fix bug (not calendar)

--- src/pages/MainLayout/CalendarPage/CalendarPage.jsx
+++ src/pages/MainLayout/CalendarPage/CalendarPage.jsx
@@ -1,18 +1,11 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import {
-  Route,
-  Navigate,
-  useNavigate,
-  useLocation,
-  Routes,
-} from 'react-router-dom';
+import { Route, Navigate, Routes } from 'react-router-dom';
 import { CalendarToolbar } from './CalendarToolbar';
 import { ChoosedMonth } from './CalendarMonth/ChoosedMonth';
 import { ChoosedDay } from './CalendarDay/ChoosedDay';
 import './CalendarPage.css';
+
 export const CalendarPage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const currentDate = useMemo(() => new Date(), []);
   const [tasks, setTasks] = useState([]);
 
@@ -39,19 +32,15 @@ export const CalendarPage = () => {
       <div className="calendar-divider" />
       <div className="calendar-content">
         <Routes>
-          <Route path="/calendar/month/:currentDate">
-            <div className="calendar-route">
-              <ChoosedMonth tasks={tasks} />
-            </div>
-          </Route>
-          <Route path="/calendar/day/:selectedDate">
-            <div className="calendar-route">
-              <ChoosedDay />
-            </div>
-          </Route>
-          <Route path="/calendar">
-            <Navigate to={`/calendar/month/${currentDate}`} />
-          </Route>
+          <Route
+            path="/calendar/month/:currentDate"
+            element={<ChoosedMonth tasks={tasks} />}
+          />
+          <Route path="/calendar/day/:selectedDate" element={<ChoosedDay />} />
+          <Route
+            path="/calendar"
+            element={<Navigate to={`/calendar/month/${currentDate}`} />}
+          />
         </Routes>
       </div>
     </div>
