3a0ed8a40af216d320477421030de7da9e2e6406
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-08-14 15:31:01 +0300

    togle_password+Feedback_form

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,9 +1,44 @@
-
+import css from './FeedbackForm.module.css';
+import sprite from 'images/sprite.svg'
 
 export default function FeedbackForm() {
     return (
-        <div className={css.container}>
-
+        <div className={css.feetback_container}>
+            <div className={css.container}>
+                <button className={css.button_close} type="button">
+                    <svg className={css.icon} width='24' height='24'>
+                        <use href={sprite + '#icon-x-close'}></use>
+                    </svg>
+                </button>
+                <button className={css.button_edit} type="button">
+                    <svg className={css.icon} width='24' height='24'>
+                        <use href={sprite + '#icon-x-close'}></use>
+                    </svg>
+                </button>
+                <button className={css.button_remove} type="button">
+                    <svg className={css.icon} width='24' height='24'>
+                        <use href={sprite + '#icon-x-close'}></use>
+                    </svg>
+                </button>
+                <form className={css.form}>
+                    <label className={css.label}>Rating
+                        <input 
+                        className={css.input}
+                        type="text" 
+                        placeholder='ЗАГЛУШКА (МАЮТЬ БУТИ ЗІРОЧКИ)' /> 
+                    </label>
+                    <label className={css.label}>Review
+                        <input 
+                            className={css.input}
+                            type="text"
+                            placeholder='Enter text' />
+                    </label>
+                </form>
+                <div className={css.button_container}>                
+                    <button className={css.button_save} type="submit">Save</button>
+                    <button className={css.button_cancel} type="submit">Cancel</button>
+                </div>
+            </div>
         </div>
     )
 };
