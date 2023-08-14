71b518efdf375fb422d401d9ca56a036253eb431
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-08-14 17:02:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,15 @@
 // import { MainPage } from 'pages/MainPage/MainPage';
 import './app.css';
-import { AppRoutes  } from './Routes/Routes';
+import { AppRoutes } from './Routes/Routes';
 import { HeaderWork } from './WORK-file/HeaderWork';
 
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { Loader } from './Loader/Loader-all';
 
 export const App = () => {
-
   // useEffect(() => {
 
-
   // })
-
 
   return (
     <>
