e184ce6edf2718f2eb3a8aab98e2d95bfdeeef7f
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-08-14 18:54:09 +0300

    add refresh token

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,22 @@ import './app.css';
 import { AppRoutes } from './Routes/Routes';
 import { HeaderWork } from './WORK-file/HeaderWork';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Loader } from './Loader/Loader-all';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectToken } from 'redux/auth/selectors';
+import { refresh } from 'redux/auth/operations';
 
 export const App = () => {
-  // useEffect(() => {
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
 
-  // })
+// Перевірка що токен валідний
+  useEffect(() => {
+    dispatch(refresh());
+  }, [dispatch, token]);
+
+
 
   return (
     <>
