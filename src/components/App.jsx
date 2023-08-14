d7251decbd85bc0eed8baf5d4c2c980e46f96316
Svitlana Shvydanenko <Svik_Svik@ukr.net>
2023-08-15 02:36:22 +0300

    loader на оновлення(рефреш) сторінки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,42 @@
 // import { MainPage } from 'pages/MainPage/MainPage';
-import { refresh } from 'redux/auth/operations';
+import React, {useState, useEffect} from 'react';
 import './app.css';
-import { Loader } from './Loader/Loader';
 import { AppRoutes } from './Routes/Routes';
 import { HeaderWork } from './WORK-file/HeaderWork';
+import BeatLoader from "react-spinners/BeatLoader";
 
-import { Suspense, useEffect } from 'react';
-import {  useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/selectors';
 
 
-export const App = () => {
- const dispatch = useDispatch() 
-  
+export const App = () => { 
+let [loading, setLoading] = useState(false);
+useEffect(() => {
+setLoading(true);
+setTimeout(() => {
+setLoading(false);
+},1500)
+}, []);
 
   const token = useSelector(selectToken);
 
 // Перевірка що токен валідний
-  useEffect(() => {
-    dispatch(refresh());
-  }, [dispatch, token]);
+  // useEffect(() => {
+  //   dispatch(refresh());
+  // }, [dispatch, token]);
 
-
-
+  
   return (
     <>
-      <Suspense fallback={<Loader />}>
+      {loading ? (
+        <div class="loader">
+          <BeatLoader color={'#3E85F3'} loading={loading} size={30} />
+        </div>
+      ) : (
         <HeaderWork />
-        <AppRoutes />
-        {/* <MainPage/> */}
-      </Suspense>
+      )}
+      <AppRoutes />
+      {/* <MainPage/> */}
     </>
   );
-};
+}
