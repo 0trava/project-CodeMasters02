ed2245b8b7aa66c7f2aa4f6720f5abf2b6e7ce91
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-08-15 18:44:13 +0300

    fix
    fix conflict

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -1,19 +1,20 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import { SideBarPage } from 'pages/SideBarPage/SideBarPage';
 import './MainLayout.css';
-/*
-import
-{
-  Navigation,
-  Block, LogoBlock, LogoText, Text, PanelText,
-  PageBlock, PagePanel, PageSideBar, PageSideBarText,
-  Button, ButtonText,
-  FeedbackBlock, ButtonFeedbackText, AvatarUsers
-}
-from './MainLayout.styled';
-*/
+import { Header } from 'components/Header/Header';
+// import { ROUTES } from 'utils/routes';
+
 export const MainLayout = () => {
+  // const navigate = useNavigate();
+
+  // Перевірка що токен валідний
+  useEffect(() => {
+    // navigate(ROUTES.CALENDAR)
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="MainLayout__container">
       <header className="">
@@ -24,7 +25,9 @@ export const MainLayout = () => {
         </aside>
       </header>
       <div>
-        <div className="MainLayout__header">{/* Header */}</div>
+        {/* Header */}
+        <Header />
+
         <div className="MainLayout__block">
           <Suspense fallback={<div>Loading page...</div>}>
             <Outlet />
