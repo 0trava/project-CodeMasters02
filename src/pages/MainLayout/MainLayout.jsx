30fc05587b7ced5fba3b1650f5278abefb54a496
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-08-15 12:48:22 +0300

    add routes on start to /calendar/*

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -1,11 +1,20 @@
-import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { Suspense, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import { SideBarPage } from 'pages/SideBarPage/SideBarPage';
 import './MainLayout.css';
 import { Header } from 'components/Header/Header';
+import { ROUTES } from 'utils/routes';
 
 export const MainLayout = () => {
+  const navigate = useNavigate();
+
+  // Перевірка що токен валідний
+  useEffect(() => {
+    navigate(ROUTES.CALENDAR)
+  },);
+
+    
   return (
     <div className="MainLayout__container">
       <header className="">
