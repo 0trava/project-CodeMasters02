88b11119aaabefa3a8f753d98bd20e8a5cf77c67
Demag0g1 <konievanatol@gmail.com>
2023-08-17 12:06:11 +0300

    half of work

--- src/pages/MainLayout/CalendarPage/PeriodPaginator.jsx
+++ src/pages/MainLayout/CalendarPage/PeriodPaginator.jsx
@@ -4,30 +4,31 @@ import { format, addMonths,  } from 'date-fns';
 import './PeriodPaginator.css';
 import sprite from '../../../images/sprite.svg';
 
-export const PeriodPaginator = ({ selectedDate, setSelectedDate, changeDate }) => {
+export const PeriodPaginator = ({ selectedDate, setSelectedDate }) => {
   const changeDate = amount => {
     setSelectedDate(prevDate => {
       return addMonths(prevDate, amount);
     });
   };
-  const handleDateChange = (amount) => {
+
+  const handleDateChange = amount => {
     changeDate(amount); 
   };
-  };
+
   const periodFormat = () => {
     return format(selectedDate, 'MMMM yyyy').toUpperCase();
   };
-console.log(selectedDate);
+
   return (
     <div className="dateWrapper">
       <div className="currentDate">{periodFormat()}</div>
       <div>
-        <button className="chevronLeftBtn" onClick={() =>  handleDateChange(-1)}>
+        <button className="chevronLeftBtn" onClick={() => handleDateChange(-1)}>
           <svg className="icon-chevron" width="16" height="16">
             <use href={sprite + '#icon-chevron-left'}></use>
           </svg>
         </button>
-        <button className="chevronRightBtn" onClick={() =>handleDateChange(1)}>
+        <button className="chevronRightBtn" onClick={() => handleDateChange(1)}>
           <svg className="icon-chevron" width="16" height="16">
             <use href={sprite + '#icon-chevron-right'}></use>
           </svg>
