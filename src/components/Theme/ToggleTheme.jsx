7f29c33da52148ea3043a496f9e4b9f28b8f760c
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-08-19 00:00:49 +0300

    favicon_fix

--- src/components/Theme/ToggleTheme.jsx
+++ src/components/Theme/ToggleTheme.jsx
@@ -7,7 +7,8 @@ import css from './ToggleTheme.module.css';
 export const ToggleTheme = () => {
   const [theme, setTheme] = useState(false);
   const dispatch = useDispatch();
-
+  const headElem = document.querySelector('head');
+  const elem = headElem.firstElementChild;
   useEffect(() => {
     console.log('Theme - ', theme);
   }, [theme]);
@@ -17,10 +18,12 @@ export const ToggleTheme = () => {
       setTheme(false);
       document.body.style.backgroundColor = 'white';
       document.body.style.color = '#171820';
+      elem.href = "/project-CodeMasters02/30893.svg";
     } else {
       setTheme(true);
       document.body.style.backgroundColor = '#171820';
       document.body.style.color = 'white';
+      elem.href = "/project-CodeMasters02/2024039.svg";
     }
     dispatch();
   }
