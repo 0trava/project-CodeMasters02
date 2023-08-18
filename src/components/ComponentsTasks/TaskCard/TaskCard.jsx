88b78e75c7951b6cbfb5711394ff3b486a28a602
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-08-18 23:51:40 +0300

    add TaskCard

--- src/components/ComponentsTasks/TaskCard/TaskCard.jsx
+++ src/components/ComponentsTasks/TaskCard/TaskCard.jsx
@@ -1,3 +1,42 @@
+import './TaskCard.css'
+import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/selectors';
+import { EditBtnMenu } from '../EditBtnMenu/EditBtnMenu';
+
+
+
 export const TaskCard = () => {
-  return <></>;
+  const { name, avatar } = useSelector(selectUser);
+
+  return (
+  <div className="TaskCard__container">
+    <p className="TaskCard__title">Read and respond to emails and messages...</p>
+    
+    <div className='TaskCard__info'>
+
+
+    <div  className='TaskCard__block'>
+      {/* Avatar */}
+      <div className="TaskCard__avatar">
+          {avatar ? (
+            <img src={avatar} alt="reviewImg" key="reviewImg" />
+          ) : (
+            <p>{name[0]}</p>
+          )}
+        </div>
+      
+      {/* Task Status */}
+      <div className='TaskCard__status'>
+        <p>TaskCard</p>
+      </div>
+
+
+    </div>
+      {/* Buttons */}
+      <EditBtnMenu className="TaskCard__btn"/>
+    </div>
+
+
+  </div>
+  );
 };
--- src/pages/MainLayout/CalendarPage/CalendarDay/CalendarDay.jsx
+++ src/pages/MainLayout/CalendarPage/CalendarDay/CalendarDay.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { DayCalendarHead } from './DayCalendarHead/DayCalendarHead';
+import { TaskCard } from 'components/ComponentsTasks/TaskCard/TaskCard';
 
 export const CalendarDay = () => {
   return (
     <div className="day-calendar-page">
       <DayCalendarHead />
-
+      <TaskCard/>
     </div>
   );
 };
