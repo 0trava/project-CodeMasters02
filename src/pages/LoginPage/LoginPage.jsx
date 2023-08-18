cf6119e946c13f2b8b16e5f799cc65f2140a8c9a
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-08-19 00:41:04 +0300

    TaskForm

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,12 +2,10 @@ import LoginForm from 'components/LoginForm/LoginForm';
 import React from 'react';
 import css from './LoginPage.module.css'
 import AuthNavigate from 'components/AuthNavigate/AuthNavigate';
-import { TaskForm } from 'components/TaskForm/TaskForm';
 
 export const LoginPage = () => {
   return (
     <div className={css.login_page} >
-      <TaskForm />
       <LoginForm />
       <AuthNavigate route={'/register'} pageName={'Sign up'} />
     </div>
