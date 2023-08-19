a7f438a6b7c6cc74b8a436345f46ec49749a9be7
Oleksandr1301 <viss82@ukr.net>
2023-08-19 13:20:43 +0300

    add color

--- src/components/StatisticksComponents/SimpleBarChart/SimpleBarChart.jsx
+++ src/components/StatisticksComponents/SimpleBarChart/SimpleBarChart.jsx
@@ -49,8 +49,8 @@ export const StatisticsPage = () => {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
+      <Bar dataKey="pv" fill="#ffd2dd" />
+      <Bar dataKey="uv" fill="#3E85F3" />
     </BarChart>
   );
 }
