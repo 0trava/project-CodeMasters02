188fcb1195d96101999b350cb4e42a7f57362396
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-08-20 15:25:59 +0300

    fix_styles_bug

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './FeedbackForm.css';
 import { useState } from 'react';
-import { RiPencilLine } from 'react-icons/ri';
-import { RiDeleteBinLine } from 'react-icons/ri';
 import { RiCloseLine } from 'react-icons/ri';
 import { useSelector } from 'react-redux';
 import { selectUserReview } from 'redux/reviews/selectors';
 import { useDispatch } from "react-redux";
 import { addReview, deleteReview, editReview } from 'redux/reviews/operations';
+import sprite from '../../images/sprite.svg';
 
 export const FeedbackForm =  ({ onClose }) => {
   const dispatch = useDispatch();
@@ -116,11 +115,15 @@ export const FeedbackForm =  ({ onClose }) => {
 
           {Review ? (
             <div className="FeedbackForm__btn-changeblock">
-              <button onClick={handleChage}className="btn_icon_pencil">
-                <RiPencilLine className="icon_pencil" />
+              <button onClick={handleChage} className="btn_icon_pencil">
+                <svg className="iconPencil" width="16" height="16">
+                  <use href={sprite + '#icon-pencil'}></use>
+                </svg>
               </button>
               <button onClick={toDelete} className="btn_icon_trash" >
-                <RiDeleteBinLine className="icon_trash"/>
+                <svg width="16" height="16">
+                  <use href={sprite + '#icon-trash-2'}></use>
+                </svg>
               </button>
             </div>
           ) : (
