373fc70abc88023025cf87501b7c3d960c5e4027
Skripnuyk <walker8613@gmail.com>
2023-08-20 19:12:16 +0300

    Зробив розмітку хеадеру у статистиці

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import css from './Header.module.css';
 import sprite from '../../images/sprite.svg';
+import logo from '../../images/goose-calendar1x.png';
+import logo2x from '../../images/goose-calendar2x.png';
 import { useLocation } from 'react-router-dom';
 import { ToggleTheme } from './ThemeToggler/ToggleTheme';
 import { FeedbackBtn } from 'components/Header/FeedbackBtn/FeedbackBtn';
@@ -7,10 +9,14 @@ import { UserInfo } from './UserInfo/UserInfo';
 import { FeedbackModal } from './FeedbackModal/FeedbackModal';
 import { useState } from 'react';
 import { ModalSideBar } from 'components/Modal/ModalSideBar';
+import { selectTasks } from '../../redux/tasks/selectors';
+import { useSelector } from 'react-redux';
 
 export const Header = () => {
   const [showModal, setShowModal] = useState(false);
   const [showSideBar, setShowSideBar] = useState(false);
+
+  const showGooseImage = useSelector(selectTasks);
 
   // Modal for Review
   const openModal = () => {
@@ -46,7 +52,21 @@ export const Header = () => {
   return (
     <>
       <div className={css.wrapper}>
-        <h1 className={css.title}>{title}</h1>
+        {showGooseImage ? <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet={`${logo} 1x, ${logo2x} 2x`}
+            />
+            <img src={logo} alt="Logo goose" />
+          </picture>
+
+          <h1 className={css.title}>{title}</h1>
+          <h2>
+            <span>Let go</span> of the past and focus on the present!
+          </h2>
+        </div> :
+        <h1 className={css.title}>{title}</h1>}
         <button type="button" className={css.btn}>
           <svg className={css.menuBurger} onClick={openSideBar}>
             <use href={`${sprite}#icon-burger`} />
@@ -59,9 +79,7 @@ export const Header = () => {
         </div>
       </div>
       {showModal && <FeedbackModal onClose={closeModal} />}
-      {showSideBar ? <ModalSideBar setShowSideBar={closeSideBar}/> : ""}
-      
-
+      {showSideBar ? <ModalSideBar setShowSideBar={closeSideBar} /> : ''}
     </>
   );
 };
