91255d06af41c0de8a2f7a32f5d0461e45c0d69e
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-08-20 10:38:07 +0300

    .

--- src/components/Theme/ToggleTheme.jsx
+++ src/components/Theme/ToggleTheme.jsx
@@ -5,25 +5,43 @@ import sprite from '../../images/sprite.svg';
 import css from './ToggleTheme.module.css';
 
 export const ToggleTheme = () => {
-  const [theme, setTheme] = useState(false);
+
+  const [ theme, setTheme ] = useState(false);
   const dispatch = useDispatch();
+  
   const headElem = document.querySelector('head');
-  const elem = headElem.firstElementChild;
+  const linkElem = headElem.firstElementChild;
+  const lightFaviconPatch = '/project-CodeMasters02/favicon.png';
+  const darkFaviconPatch = '/project-CodeMasters02/favicon_dark.svg';
+
   useEffect(() => {
     console.log('Theme - ', theme);
-  }, [theme]);
+    function updateFavicon(themeDarkLight) {
+      if (themeDarkLight.matches) {
+        linkElem.href = darkFaviconPatch;
+      }
+      else {
+        linkElem.href = lightFaviconPatch;
+      }
+    }
+    const themeObj = window.matchMedia("(max-width: 768px)");
+ // const themeObj = window.matchMedia("(prefers-color-scheme: dark)");
 
+    updateFavicon(themeObj);
+
+    themeObj.addListener(updateFavicon);
+
+  }, [theme]);
   function handleClick() {
     if (theme === true) {
       setTheme(false);
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = '#171820';
-      elem.href = "/project-CodeMasters02/30893.svg";
-    } else {
+      document.body.style.backgroundColor = '#fff';
+      document.body.style.color = '#181921';
+    }
+    else {
       setTheme(true);
-      document.body.style.backgroundColor = '#171820';
-      document.body.style.color = 'white';
-      elem.href = "/project-CodeMasters02/2024039.svg";
+      document.body.style.backgroundColor = '#181921';
+      document.body.style.color = '#fff';
     }
     dispatch();
   }
@@ -34,9 +52,11 @@ export const ToggleTheme = () => {
           <use href={sprite + '#icon-sun'}></use>
         </svg>
       ) : (
+        <>
         <svg className={css.icon} width="32" height="32">
           <use href={sprite + '#icon-moon'}></use>
         </svg>
+        </>
       )}
     </ButtonToggle>
   );
